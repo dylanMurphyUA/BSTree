32d10832394ed04c64e9d854aa6500d679640f87
Unknown <dmurphy5@albany.edu>
2017-04-30 13:09:36 -0400

    TNode done Tree started

--- TNode.java
+++ TNode.java
@@ -1,4 +1,43 @@
 
 public class TNode {
+	TNode left, right;
+    String data;
+    
+    public TNode(){
+    		left = null;
+        right = null;
+        data = null;
+    }
+    public TNode(String s){
+    		left = null;
+        right = null;
+        data = s;
+    }
+    public TNode getLeft(){
+    	return left;
+    }
+    
+    public TNode getRight(){
+    	return right;
+    }
+    
+    public void setLeft(TNode t)
+    {
+        left = t;
+    }
 
+    public void setRight(TNode t)
+    {
+        right = t;
+    }
+
+    public void setData(String d)
+    {
+        data = d;
+    }
+
+    public String getData()
+    {
+        return data;
+    }
 }
--- Tree.java
+++ Tree.java
@@ -1,4 +1,18 @@
 
 public class Tree {
-
+	private TNode root;
+	
+	public Tree(){
+		root=null;
+	}
+	public boolean isEmpty(){
+		return root == null;
+	}
+	public void setRoot(String data){
+		root = new TNode(data);
+	}
+	public TNode getRoot(){
+		return root;
+	}
+	
 }
